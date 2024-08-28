c9fa0f53a2ba97fb0e43caeec1773cf1fcb6db42
Tharun Atithya <tharunaadhi6@gmail.com>
2024-08-28 11:59:07 +0530

    commit

--- cnn_model.py
+++ cnn_model.py
@@ -1,28 +1,29 @@
+
 import tensorflow as tf
+import time
 import streamlit as st
 from PIL import Image
 import numpy as np
 from keras.preprocessing import image
-
 model = tf.keras.models.load_model("mobilenet.h5")
 indives = {0:'Brown_rust', 1:'Healthy', 2:'Yellow_rust'}
-
 st.title("Image prediction model")
-
-st.image(r"C:\Users\tharu\OneDrive\Pictures\Screenshots 1\Screenshot 2024-07-13 144102.png", "Beloved company")
-
+st.logo(r"C:\Users\tharu\OneDrive\Pictures\Screenshots 1\Screenshot 2024-07-13 144102.png")
 raw_data = st.file_uploader("choose an image file", accept_multiple_files=False)
-
 if raw_data is not None:
     img = Image.open(raw_data)
     img = img.resize((224, 224)) 
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-
+    st.image(raw_data, "uploaded image")
     def predict():
         result = (model.predict(img_array))[0]
         for i,j in enumerate(result):
             if j == 1:
-                st.success(indives[i])
-        
+                resul = indives[i]
+                with st.spinner('Your Image is cooking🦇'):
+                    time.sleep(2)
+                with st.container():
+                    st.success(indives[i])
+
     st.button("predict", on_click=predict)
